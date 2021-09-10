ab802e2f678519e67ca765036518ee236719f0f9
Andrelino Silva <andrelinocom@gmail.com>
2021-09-09 22:02:50 -0300

    Estilizando tela cadastro

--- web/src/pages/signup/index.tsx
+++ web/src/pages/signup/index.tsx
@@ -5,28 +5,32 @@ import styles from './styles.module.scss';
 export default function SignUp(): JSX.Element {
   return (
     <div className={styles.container}>
+      <h1>Cadastro</h1>
       <form>
         <label>
-          Nome completo
+          Nome completo:
           <input type="text" />
         </label>
         <label>
-          E-mail
+          E-mail:
           <input type="email" />
         </label>
         <label>
-          Digite uma senha
+          Digite uma senha:
           <input type="password" />
         </label>
         <label>
-          Confirme sua senha
+          Confirme sua senha:
           <input type="password" />
         </label>
         <label>
-          Força da senha
+          Força da senha:
           <input />
         </label>
       </form>
+      <button type="submit" className={styles.button}>
+        Cadastrar
+      </button>
     </div>
   );
 }
